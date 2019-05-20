bb59c4959e55f8c49dd2a485689d39544c8f2ff9
taka0213 <snowfaceweapon@gmail.com>
2019-05-20 21:19:14 +0900

    自動更新機能

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,51 +1,70 @@
-$(function(){
-    function buildHTML(message){
-     var imagehtml = message.image == null ? "" : `<img src="${message.image}" class="lower-message__image">`
-      var html = `<div class=message>
-                      <div class="upper-message">
-                        <div class="upper-message__user-name">
-                        ${message.user_name}
-                        </div>
-                        <div class="upper-message__date">
-                        ${message.created_at}
-                        </div>
-                      </div>
-                      <div class="lower-message">
-                        <p class="lower-message__content">
-                        ${message.content}
-                        </p>
-                        ${imagehtml}
-                      </div>
-                    </div> `
-      return html;
-    }
-    function form_reset() {
-      $('#new_message')[0].reset();
-  }
-  
-    $('#new_message').on('submit', function(e){
-      e.preventDefault();
-      var formData = new FormData(this);
-      var href = window.location.href
-      $.ajax({
-        url: href,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      })
-      .done(function(data){
-        var html = buildHTML(data);
-         $('.messages').append(html);
-         $( ".form__submit").prop( "disabled", false );  
-         $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-         $('.form__message').reset();
-         $('.hidden').reset();
-         })
-     .fail(function(){
-        alert('error');
-      })
-     })
+$(function() {
+  var buildMessageHTML = function(message) {
+    var image = message.image.url ? `<img src="${message.image.url}" class="lower-message__image" />` : "";
+    var html = `<div class="message new-message" data-message_id="${message.id}">
+                  <div class="info">
+                    <p class="info__user">${message.user_name}</p>
+                    <p class="info__date">${message.created_at}</p>
+                  </div>
+                  <div class="message__text">
+                    <p class="lower-message__content">${message.content}</p>
+                    ${image}
+                  </div>
+                </div>`
+    return html;
+    };
+
+  $('#new_message').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildMessageHTML(data);
+      $('.messages').append(html)
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $('form')[0].reset();
+    })
+    .fail(function(){
+      alert('error');
     });
-  
+  });
+
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data('message_id');
+    path = location.pathname;
+    url = path.replace('messages', 'api/messages')
+    $.ajax({
+      url: url,
+      type: 'GET',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      var insartHTML = '';
+      messages.forEach(function(message){
+          insartHTML = buildMessageHTML(message);
+          $(".messages").append(insartHTML);
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      });
+    })
+    .fail(function() {
+    });
+  };
+
+  $(function(){
+  if (location.pathname.match(/messages/)) {
+    setInterval(reloadMessages, 5000);
+  }
+  else {
+    clearInterval(reloadMessages);
+  };
+  });
+});
